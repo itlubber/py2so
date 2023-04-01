18d58ce12f34bbcac0c58a6a9b9506b93fb2b221
itlubber <39397838+itlubber@users.noreply.github.com>
2023-04-01 17:30:03 +0800

    替换为生产环境编译代码

--- py2so.py
+++ py2so.py
@@ -1,99 +1,71 @@
-# -*- coding: utf-8 -*-
-"""
-@Time    : 2022/8/23 13:12
-@Author  : itlubber
-@Site    : itlubber.art
-"""
-
 import os
+import shutil
 import sys
 import time
-import shutil
-import traceback
 from distutils.core import setup
 from Cython.Build import cythonize
 
 
+starttime = time.time()
 currdir = os.path.abspath('.')
+parentpath = sys.argv[1] if len(sys.argv)>1 else ""
+setupfile= os.path.join(os.path.abspath('.'), __file__)
 build_dir = "build"
 build_tmp_dir = build_dir + "/temp"
 
 
-def build_list(basepath, name='', excepts=[], copyOther=False, delC=False, starttime=None):
+def getpy(basepath=os.path.abspath('.'), parentpath='', name='', excepts=(), copyOther=False,delC=False):
     """
     获取py文件的路径
     :param basepath: 根路径
+    :param parentpath: 父路径
     :param name: 文件/夹
+    :param excepts: 排除文件
     :param copy: 是否copy其他文件
-    :param excepts: 不拷贝的文件
     :return: py文件的迭代器
     """
-    excepts_files = [os.path.join(currdir, f) for f in excepts]
-    fullpath = os.path.join(basepath, name)
+    fullpath = os.path.join(basepath, parentpath, name)
     for fname in os.listdir(fullpath):
         ffile = os.path.join(fullpath, fname)
-        
         if os.path.isdir(ffile) and fname != build_dir and not fname.startswith('.'):
-            for f in build_list(basepath, name=fname, copyOther=copyOther, delC=delC, starttime=starttime):
-                yield os.path.join(ffile, f)
-                
+            for f in getpy(basepath, os.path.join(parentpath, name), fname, excepts, copyOther, delC):
+                if f not in excepts:
+                    yield f
+                else:
+                    pass
         elif os.path.isfile(ffile):
             ext = os.path.splitext(fname)[1]
             if ext == ".c":
                 if delC and os.stat(ffile).st_mtime > starttime:
                     os.remove(ffile)
-            elif os.path.splitext(fname)[1] not in('.pyc', '.pyx'):
-                if os.path.splitext(fname)[1] in ('.py', '.pyx') and not fname.startswith('__'):
-                    yield os.path.join(name, fname)
-                elif ffile not in excepts_files and copyOther:
-                    dstdir = os.path.join(basepath, build_dir, name)
-                    if not os.path.isdir(dstdir):
-                        os.makedirs(dstdir)
-                    shutil.copyfile(ffile, os.path.join(dstdir, fname))
+            elif ffile not in excepts and os.path.splitext(fname)[1] not in('.pyc', '.pyx'):
+                if os.path.splitext(fname)[1] in('.py', '.pyx') and not fname.startswith('__'):
+                    yield os.path.join(parentpath, name, fname)
+                elif copyOther:
+                        dstdir = os.path.join(basepath, build_dir, parentpath, name)
+                        if not os.path.isdir(dstdir): 
+                            os.makedirs(dstdir)
+                        shutil.copyfile(ffile, os.path.join(dstdir, fname))
         else:
             pass
-        
-        
-def copy_file(source):
-    dist_dir = os.path.join(currdir, build_dir, source)
-    source_dir = os.path.join(currdir, source)
-    
-    if not os.path.isdir(source_dir):
-        if not os.path.exists(os.path.dirname(dist_dir)):
-            os.makedirs(os.path.dirname(dist_dir))
-    
-        shutil.copyfile(source_dir, dist_dir)
-    else:
-        for file in os.listdir(source_dir):
-            copy_file(os.path.join(source_dir, file))
 
 
 if __name__ == '__main__':
-    starttime=time.time()
-    
-    # 不编译的 python 文件
-    excepts_build = ["py2so.py", "main.py"]
-    # 不复制到编译后的非 python 文件
-    excepts_files = ["README.md", "LICENSE", ".gitignore", "setup.py"]
+    excepts_file = (setupfile)
 
+    # 编译 .py 文件
     try:
-        # 编译 python 文件
-        module_list = list(build_list(currdir, starttime=starttime))
-        module_list = sorted([py for py in module_list if py not in excepts_build], key=lambda f: os.path.getsize(f))
-        setup(ext_modules=cythonize(module_list, language_level="3"), script_args=["build_ext", "-b", build_dir, "-t", build_tmp_dir])
+        module_list = list(getpy(basepath=currdir, parentpath=parentpath, excepts=excepts_file))
+        setup(ext_modules = cythonize(module_list),script_args=["build_ext", "-b", build_dir, "-t", build_tmp_dir])
+    except Exception as e:
+        print (e)
 
-        # 拷贝其他文件
-        list(build_list(currdir, excepts=excepts_files, copyOther=True, starttime=starttime))
+    # 拷贝其他文件
+    list(getpy(basepath=currdir, parentpath=parentpath, excepts=excepts_file, copyOther=True))
 
-        # 拷贝不编译的py文件
-        for file in excepts_build:
-            copy_file(file)
+    # 删除编译产生的中间文件
+    module_list = list(getpy(basepath=currdir, parentpath=parentpath, excepts=excepts_file, delC=True))
+    if os.path.exists(build_tmp_dir):
+        shutil.rmtree(build_tmp_dir)
 
-        print("complate! time:", time.time() - starttime, 's')
-    except:
-        traceback.print_exc()
-    finally:
-        # 删除编译产生的中间文件
-        module_list = list(build_list(currdir, delC=True, starttime=starttime))
-        if os.path.exists(build_tmp_dir):
-            shutil.rmtree(build_tmp_dir)
+    print ("complate! time:", time.time()-starttime, 's')
